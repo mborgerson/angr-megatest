4fa33b059b67728d098b245f94c51088d0fe5e07
Matt Borgerson <contact@mborgerson.com>
2020-02-07 17:57:28 -0700

    Move rename binster dockerfile and add shebang to list.sh

--- list.sh
+++ list.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker run -it --rm binster bash -c "lz4cat /var/lib/apt/lists/*.lz4 | grep ^Filename | grep dbgsym" |
 	awk '{print $2}' | sed -e "s|.*/main/||" > /tmp/pkgs-$$
 fromdos /tmp/pkgs-$$
